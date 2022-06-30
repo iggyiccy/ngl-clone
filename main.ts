3060306b78f34220ce094af4982481d6f0ef2632
iggyi <iccyn@pm.me>
2022-06-30 16:55:39 +1000

    init 2

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
+#!/usr/bin/env -S deno run --allow-read --allow-net --allow-env --allow-run --allow-hrtime --watch=routes/
+
 /// <reference no-default-lib="true" />
 /// <reference lib="dom" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
+import "$std/dotenv/load.ts";
+
 import { InnerRenderFunction, RenderContext, start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-import { config, setup } from "@twind";
 import { virtualSheet } from "twind/sheets";
+import { config, setup } from "@twind";
 
 const sheet = virtualSheet();
 sheet.reset();
